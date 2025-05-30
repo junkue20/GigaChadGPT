fc650e32f1e88d1bcd8dccdaecabc5753e17f99e
junkue20 <junkue17@naver.com>
2025-05-31 02:49:04 +0900

    봇 프로필 아이콘 크기조정

--- app/chat/components/ChatBotIcon.tsx
+++ app/chat/components/ChatBotIcon.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export function ChatBotIcon() {
   return (
-    <div className="w-14 h-14 rounded-full flex-shrink-0 relative overflow-hidden">
+    <div className="w-12 h-12 rounded-full flex-shrink-0 relative overflow-hidden">
       <Image
         src="/gigachad-face.jpg"
         alt="기가채드 프로필 아이콘"
