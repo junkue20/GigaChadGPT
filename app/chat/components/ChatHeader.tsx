7b51dd9f9506255328dff6a0c359e05fff252265
junkue20 <junkue17@naver.com>
2025-05-31 02:49:21 +0900

    사이드바 메뉴 아이콘 스타일 조정

--- app/chat/components/ChatHeader.tsx
+++ app/chat/components/ChatHeader.tsx
@@ -1,9 +1,17 @@
 'use client';
 
-export default function ChatHeader() {
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { ChatContainerProps } from '../types';
+import ChatBotIcon from './ChatBotIcon';
+
+export default function ChatHeader({ onMenuClick }: ChatContainerProps) {
   return (
-    <header className="hidden md:flex p-4 border-gray-700">
-      <h1 className="font-bold text-xl">기가채드와의 대화</h1>
+    <header className="p-4 flex items-center gap-4 border-b border-gray-700">
+      <button onClick={onMenuClick} className="p-lg">
+        <HamburgerMenuIcon className="sm:w-6 sm:h-6 md:w-4 md:h-4" />
+      </button>
+      <ChatBotIcon />
+      <h1 className="font-bold text-lg">기가채드</h1>
     </header>
   );
 }
